7a919927310f6cfab2a7c7ae669ce5c1195f22b6
Alex Rock Ancelet <pierstoval@gmail.com>
2025-03-21 07:28:21 +0200

    -

---
+++ tests/Twig/ActivityExtensionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Tests\Twig;
+
+use App\DataFixtures\AttendeeFixture;
+use App\DataFixtures\ScheduledActivityFixture;
+use App\Entity\ScheduledActivity;
+use App\Repository\ScheduledActivityRepository;
+use App\Tests\TestUtils\GetUser;
+use App\Twig\Extension\ActivityExtension;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ActivityExtensionTest extends KernelTestCase
+{
+    use GetUser;
+
+    #[DataProvider('provideData')]
+    public function testBasicIntegration(string $username, string $activityId, bool $expectedResult): void
+    {
+        self::bootKernel();
+        /** @var ActivityExtension $extension */
+        $extension = self::getContainer()->get(ActivityExtension::class);
+
+        self::assertSame('is_user_registered_to_activity', $extension->getFunctions()[0]->getName());
+
+        $user = $this->getUser($username);
+        /** @var ScheduledActivity $activity */
+        $activity = self::getContainer()->get(ScheduledActivityRepository::class)->find($activityId);
+
+        $result = $extension->isUserRegisteredToActivity($user, $activity);
+
+        self::assertSame($expectedResult, $result);
+    }
+
+    public static function provideData(): iterable
+    {
+        foreach (AttendeeFixture::getStaticData() as $data) {
+            $username = \preg_replace('~^user-(.+)$~', '$1', $data['registeredBy']->name);
+            $activityId = \preg_replace('~^scheduled-activity-(.+)$~', '$1', $data['scheduledActivity']->name);
+            yield $username.' '.$activityId => [
+                $username,
+                $activityId,
+                true,
+            ];
+        }
+
+        foreach (ScheduledActivityFixture::getStaticData() as $id => $data) {
+            yield 'ash'.' '.$id => ['ash', $id, false];
+            yield 'admin'.' '.$id => ['admin', $id, false];
+        }
+    }
+}
---
+++ tests/Twig/TypesExtensionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests\Twig;
+
+use App\Twig\Extension\TypesExtension;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Twig\TwigTest;
+
+class TypesExtensionTest extends KernelTestCase
+{
+    #[DataProvider('provideData')]
+    public function testBasicIntegration(mixed $input, bool $expectedResult): void
+    {
+        self::bootKernel();
+        /** @var TypesExtension $extension */
+        $extension = self::getContainer()->get(TypesExtension::class);
+
+        /** @var TwigTest $test */
+        $test = $extension->getTests()[0];
+
+        $result = $test->getCallable()($input);
+        self::assertSame($expectedResult, $result);
+    }
+
+    public static function provideData(): iterable
+    {
+        yield [1, true];
+        yield ['1', true];
+        yield ['0', true];
+        yield ['0abc', false];
+        yield ['abc', false];
+        yield [[], false];
+        yield [new \stdClass(), false];
+    }
+}
